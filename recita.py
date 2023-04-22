d0baa3c32933e32c4bbcda327463652dc35d844f
eujap <jose.ap2008@hotmail.com>
2023-04-21 21:22:30 -0400

    dia 21/04 tentativa de incluir tratamento de exceção

--- recita.py
+++ recita.py
@@ -1,29 +1,77 @@
+
 def receita ():
-    continua = []
+        
     lista = []
-
-
-    def informar_rendimento ():
-        with open ("receita.txt", "a") as arquivo:
-            arquivo.write(lista)
-            arquivo.write("\n")
-            arquivo.close
+    continua =[]
+    
 
 
     while (continua != "N"):
         temp = []
-        temp.append(input("informe o mês:  ").upper())
-        temp.append(input("informe os valor dos rendimentos R$: "))
+        temp.append(incluir_mes())
+        temp.append(informe_valor())
 
         lista = str(temp[0]) + ',' + str(temp[1])
-        print(lista, end ="\n")
-
-        informar_rendimento()
+        print(lista, end ="\n")      
+                
+        print(lista)
 
         temp.clear()
 
-        continua = input(str("informar outro mês: ").upper())
+        continua = input(str("informar outro mês? S-Sim ou N-Não:  ").upper())
+
+
+    def validar_mes(mes):
+
+
+        try:
+            num_mes = int(mes)
+            if (num_mes < 1 or num_mes > 12):
+                raise ValueError("mes de estar entre 01 e 12")
+            return True
+        except ValueError:
+            return False
+
+        
             
-    print(lista)
+    def validar_salario(valor):
+        try:
+            valor_teste = float(valor)
+            return True
+        except ValueError:
+            return False
+
+            
+    def incluir_mes():
+
+        mes = input("informe o mês (MM):  ")
+        if validar_mes(mes):
+            return mes
+        
+        else:
+            print("Por Favor! Digite um mês no formato (MM)")
+            mes = input("informe o mês (MM):  ")
 
 
+    def informe_valor():
+        valor = input("informe o salario R$:  ")
+        if validar_salario(valor):
+            return valor
+        else:
+            print("Por favor infomre um numero no formato 100.50")  
+
+    
+    def informar_receita():
+
+        with open ("receita.txt", "a+") as arquivo:
+            arquivo.write()
+            arquivo.write("\n")
+            
+            
+
+
+    
+        
+                        
+        
+receita()
